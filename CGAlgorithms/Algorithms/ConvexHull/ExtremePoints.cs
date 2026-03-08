2252f8cc96e903e9961b97fae85a7f76f2740d69
MennaaWaleed <meenna154@gmail.com>
2026-03-08 23:15:37 +0200

    Initial Templete with two algorithms implemented (Extreme Points , Extreme Segments)

---
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
@@ -0,0 +1,106 @@
+﻿using CGUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGAlgorithms.Algorithms.ConvexHull
+{
+    public class ExtremePoints : Algorithm
+    {
+        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        {
+            if (points == null)
+                return;
+            if (points.Count == 0)
+            {
+                return;
+            }
+            outPoints = new List<Point>();
+            if (points.Count == 1)
+            {
+                outPoints.Add(points[0]);
+                return;
+            }
+            if (points.Count == 2)
+            {
+                outPoints.Add(points[0]);
+                outPoints.Add(points[1]);
+                return;
+            }
+
+
+            HashSet<Point> rm = new HashSet<Point>();
+
+            for (int i = 0; i < points.Count; ++i)
+            {
+                Point p = points[i];
+
+                for (int j = 0; j < points.Count; ++j)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    Point a = points[j];
+                    for (int k = j + 1; k < points.Count; ++k)
+                    {
+                        if (i == k)
+                        {
+                            continue;
+                        }
+                        Point b = points[k];
+                        for (int l = k + 1; l < points.Count; ++l)
+                        {
+                            if (i == l)
+                            {
+                                continue;
+                            }
+                            Point c = points[l];
+
+                            if ((p.X == a.X && p.Y == a.Y) || (p.X == b.X && p.Y == b.Y) || (p.X == c.X && p.Y == c.Y))
+                            {
+                                continue;
+                            }
+
+                            var turn = HelperMethods.PointInTriangle(p, a, b, c);
+                            if (turn == Enums.PointInPolygon.Inside)
+                            {
+                                rm.Add(p);
+                                break;
+                            }
+                            else if (turn == Enums.PointInPolygon.OnEdge)
+                            {
+                                rm.Add(p);
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < points.Count; ++i)
+            {
+                if (rm.Contains(points[i]))
+                {
+                    points.RemoveAt(i);
+                    i--;
+                }
+            }
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (!outPoints.Contains(points[i]))
+                    outPoints.Add(points[i]);
+            }
+            return;
+        }
+
+        public override string ToString()
+        {
+            return "Convex Hull - Extreme Points";
+        }
+    }
+}
---
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
@@ -0,0 +1,106 @@
+﻿using CGUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGAlgorithms.Algorithms.ConvexHull
+{
+    public class ExtremeSegments : Algorithm
+    {
+        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        {
+            if (points == null)
+                return;
+            if (points.Count == 0)
+            {
+                return;
+            }
+            outPoints = new List<Point>();
+            if (points.Count == 1)
+            {
+                outPoints.Add(points[0]);
+                return;
+            }
+
+            HashSet<Point> rm = new HashSet<Point>();
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point a = points[i];
+                for (int j = i + 1; j < points.Count; j++)
+                {
+
+                    if (points[j].X == points[i].X && points[j].Y == points[i].Y) continue;
+
+                    Point b = points[j];
+                    //check if the line ab is extreme
+                    int left = 0, right = 0, colinear = 0;
+                    int numOfPoints = points.Count - 2;
+                    for (int k = 0; k < points.Count; k++)
+                    {
+
+                        if (k == i || k == j) continue;
+                        Point c = points[k];
+                        if (c.X == a.X && c.Y == a.Y || c.X == b.X && c.Y == b.Y)
+                        {
+
+                            colinear++; continue;
+                        }
+
+
+                        var turn = HelperMethods.CheckTurn(a.Vector(b), a.Vector(c));
+                        if (turn == Enums.TurnType.Left)
+                        {
+                            left++;
+                        }
+                        else if (turn == Enums.TurnType.Right)
+                        {
+                            right++;
+                        }
+                        else
+                        {
+                            colinear++;
+                            if (HelperMethods.PointOnSegment(c, a, b))
+                            {
+                                rm.Add(c);
+                            }
+                            else if (HelperMethods.PointOnSegment(a, b, c))
+                            {
+                                rm.Add(a);
+                            }
+                            else if (HelperMethods.PointOnSegment(b, a, c))
+                            {
+                                rm.Add(b);
+                            }
+                        }
+                    }
+                    if (left + colinear == numOfPoints || right + colinear == numOfPoints)
+                    {
+                        if (!outPoints.Contains(a))
+                            outPoints.Add(a);
+                        if (!outPoints.Contains(b))
+                            outPoints.Add(b);
+                    }
+                }
+            }
+            for (int i = 0; i < outPoints.Count; i++)
+            {
+                if (rm.Contains(outPoints[i]))
+                {
+                    outPoints.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            return;
+        }
+
+        public override string ToString()
+        {
+            return "Convex Hull - Extreme Segments";
+        }
+    }
+}
