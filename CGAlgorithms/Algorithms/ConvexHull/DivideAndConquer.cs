345e485abd63e746934c82014263f03da9013e3b
MennaaWaleed <meenna154@gmail.com>
2026-05-06 17:54:51 +0300

    Added Incremental, Divide and Conquer Algorithms

--- CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
+++ CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
@@ -1,23 +1,235 @@
-﻿using CGUtilities;
-using System;
+﻿using CGAlgorithms;
+using CGUtilities;
 using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CGAlgorithms.Algorithms.ConvexHull
+public class DivideAndConquer : Algorithm
 {
-    public class DivideAndConquer : Algorithm
+    public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
     {
-        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        if (points.Count < 3)
         {
-
+            outPoints = new List<Point>(points);
+            return;
         }
 
-        public override string ToString()
+        for (int i = 0; i < points.Count - 1; i++)
         {
-            return "Convex Hull - Divide & Conquer";
+            int minIndex = i;
+
+            for (int j = i + 1; j < points.Count; j++)
+            {
+                if (points[j].X < points[minIndex].X || (points[j].X == points[minIndex].X && points[j].Y < points[minIndex].Y))
+                {
+                    minIndex = j;
+                }
+            }
+
+
+            if (minIndex != i)
+            {
+                Point temp = points[i];
+                points[i] = points[minIndex];
+                points[minIndex] = temp;
+            }
+        }
+        int k = 0;
+        while (k < points.Count - 1)
+        {
+            if (points[k].Equals(points[k + 1]))
+            {
+                points.RemoveAt(k);
+            }
+
+            else
+            {
+                k++;
+            }
         }
 
+        outPoints = Divide(points);
     }
+
+
+    public static int LargestX(List<Point> points)
+    {
+        int x = 0;
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (points[i].X > points[x].X || (points[i].X == points[x].X && points[i].Y > points[x].Y))
+            {
+                x = i;
+            }
+        }
+
+        return x;
+    }
+
+    public static int SmallestX(List<Point> points)
+    {
+        int x = 0;
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (points[i].X < points[x].X || (points[i].X == points[x].X && points[i].Y < points[x].Y))
+            {
+                x = i;
+            }
+        }
+        return x;
+    }
+
+    public List<Point> Merge(List<Point> Lift, List<Point> Right)
+    {
+        int LiftLargestPoint = LargestX(Lift);
+        int RightSmallestPoint = SmallestX(Right);
+        int LeftCount = Lift.Count;
+        int RightCount = Right.Count;
+        bool Change;
+        int UpperRightIndex = RightSmallestPoint;
+        int UpperLeftIndex = LiftLargestPoint;
+        int OldRight = (RightCount + UpperRightIndex - 1) % RightCount;
+        int NextToLeft = (UpperLeftIndex + 1) % LeftCount;
+
+        do
+        {
+            do
+            {
+                if (HelperMethods.CheckTurn(new Line(Right[UpperRightIndex], Lift[UpperLeftIndex]), Lift[NextToLeft]) == Enums.TurnType.Right)
+                {
+                    UpperLeftIndex = NextToLeft; NextToLeft = (UpperLeftIndex + 1) % LeftCount;
+                    Change = false;
+                }
+                else
+                {
+                    Change = true;
+                }
+
+            } while (!Change);
+
+            if (Change == true && (HelperMethods.CheckTurn(new Line(Right[UpperRightIndex], Lift[UpperLeftIndex]), Lift[NextToLeft]) == Enums.TurnType.Colinear))
+            {
+                UpperLeftIndex = NextToLeft;
+            }
+            NextToLeft = (UpperLeftIndex + 1) % LeftCount;
+
+            while (HelperMethods.CheckTurn(new Line(Lift[UpperLeftIndex], Right[UpperRightIndex]), Right[OldRight]) == Enums.TurnType.Left)
+            {
+                UpperRightIndex = OldRight;
+                OldRight = (RightCount + UpperRightIndex - 1) % RightCount;
+
+                Change = false;
+            }
+
+            if (Change == true && (HelperMethods.CheckTurn(new Line(Lift[UpperLeftIndex], Right[UpperRightIndex]), Right[OldRight]) == Enums.TurnType.Colinear))
+            {
+                UpperRightIndex = OldRight; OldRight = (RightCount + UpperRightIndex - 1) % RightCount;
+            }
+        } while (Change == false);
+
+        int LowerLeftIndex = LiftLargestPoint;
+        int LowerRightIndex = RightSmallestPoint;
+        int OldLeft = (LeftCount + LowerLeftIndex - 1) % LeftCount;
+        int NextToRight = (LowerRightIndex + 1) % RightCount;
+
+        do
+        {
+            Change = true;
+            while (HelperMethods.CheckTurn(new Line(Right[LowerRightIndex], Lift[LowerLeftIndex]), Lift[OldLeft]) == Enums.TurnType.Left)
+            {
+                LowerLeftIndex = OldLeft;
+                OldLeft = (LeftCount + LowerLeftIndex - 1) % LeftCount;
+                Change = false;
+            }
+
+            if (Change == true && (HelperMethods.CheckTurn(new Line(Right[LowerRightIndex], Lift[LowerLeftIndex]), Lift[OldLeft]) == Enums.TurnType.Colinear))
+            {
+                LowerLeftIndex = OldLeft; OldLeft = (LeftCount + LowerLeftIndex - 1) % LeftCount;
+            }
+
+            while (HelperMethods.CheckTurn(new Line(Lift[LowerLeftIndex], Right[LowerRightIndex]), Right[NextToRight]) == Enums.TurnType.Right)
+            {
+                Change = false;
+
+                LowerRightIndex = NextToRight;
+                NextToRight = (LowerRightIndex + 1) % RightCount;
+            }
+
+            if (Change == true && (HelperMethods.CheckTurn(new Line(Lift[LowerLeftIndex], Right[LowerRightIndex]), Right[NextToRight]) == Enums.TurnType.Colinear))
+            {
+                LowerRightIndex = NextToRight; NextToRight = (LowerRightIndex + 1) % RightCount;
+            }
+        } while (Change == false);
+
+        List<Point> NewPoints = new List<Point>();
+        int ind = UpperLeftIndex;
+        if (!NewPoints.Contains(Lift[UpperLeftIndex]))
+        {
+            NewPoints.Add(Lift[UpperLeftIndex]);
+        }
+
+        while (ind != LowerLeftIndex)
+        {
+            ind = (ind + 1) % LeftCount;
+            if (!NewPoints.Contains(Lift[ind]))
+            {
+                NewPoints.Add(Lift[ind]);
+            }
+        }
+
+        ind = LowerRightIndex;
+        if (!NewPoints.Contains(Right[LowerRightIndex]))
+        {
+            NewPoints.Add(Right[LowerRightIndex]);
+        }
+
+        while (ind != UpperRightIndex)
+        {
+            ind = (ind + 1) % RightCount;
+            if (!NewPoints.Contains(Right[ind]))
+            {
+                NewPoints.Add(Right[ind]);
+            }
+        }
+
+        return NewPoints;
+    }
+
+    public List<Point> Divide(List<Point> Points)
+    {
+        if (Points.Count == 1)
+        {
+            return Points;
+        }
+
+        List<Point> r = new List<Point>();
+        List<Point> l = new List<Point>();
+
+        int i = 0;
+        do
+        {
+            l.Add(Points[i]);
+            i++;
+        } while (i < Points.Count / 2);
+
+        i = Points.Count / 2;
+        do
+        {
+            r.Add(Points[i]);
+            i++;
+        } while (i < Points.Count);
+
+        List<Point> Left = Divide(l);
+        List<Point> Right = Divide(r);
+
+        return Merge(Left, Right);
+    }
+
+
+
+    public override string ToString()
+    {
+        return "Convex Hull - Divide & Conquer";
+    }
+
 }
--- CGAlgorithms/Algorithms/ConvexHull/Incremental.cs
+++ CGAlgorithms/Algorithms/ConvexHull/Incremental.cs
@@ -12,8 +12,152 @@ namespace CGAlgorithms.Algorithms.ConvexHull
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
 
-        }
+            if (points.Count < 3)
+            {
+                outPoints = new List<Point>(points);
+                return;
+            }
 
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    if (points[j].X < points[minIndex].X ||
+                       (points[j].X == points[minIndex].X && points[j].Y < points[minIndex].Y))
+                    {
+                        minIndex = j;
+                    }
+                }
+
+
+                if (minIndex != i)
+                {
+                    Point temp = points[i];
+                    points[i] = points[minIndex];
+                    points[minIndex] = temp;
+                }
+            }
+            int k = 0;
+            while (k < points.Count - 1)
+            {
+                if (points[k].Equals(points[k + 1]))
+                    points.RemoveAt(k);
+
+                else
+                    k++;
+
+            }
+
+
+            List<Point> hull = new List<Point>();
+            hull.Add(points[0]);
+            hull.Add(points[1]);
+            hull.Add(points[2]);
+
+
+            if (HelperMethods.CheckTurn(new Line(hull[0], hull[1]), hull[2]) == Enums.TurnType.Right)
+            {
+                var temp = hull[1];
+                hull[1] = hull[2];
+                hull[2] = temp;
+            }
+
+
+            for (int i = 3; i < points.Count; i++)
+            {
+                Point p = points[i];
+
+
+                bool inside = true;
+                for (int j = 0; j < hull.Count; j++)
+                {
+                    Point a = hull[j];
+                    Point b = hull[(j + 1) % hull.Count];
+
+                    if (HelperMethods.CheckTurn(new Line(a, b), p) == Enums.TurnType.Right)
+                    {
+                        inside = false;
+                        break;
+                    }
+                }
+
+                if (inside) continue;
+
+
+                int left = -1, right = -1;
+
+                for (int j = 0; j < hull.Count; j++)
+                {
+                    Point prev = hull[(j - 1 + hull.Count) % hull.Count];
+                    Point curr = hull[j];
+                    Point next = hull[(j + 1) % hull.Count];
+
+                    var turn1 = HelperMethods.CheckTurn(new Line(prev, curr), p);
+                    var turn2 = HelperMethods.CheckTurn(new Line(curr, next), p);
+
+
+                    if ((turn1 != Enums.TurnType.Right && turn2 == Enums.TurnType.Right))
+                        left = j;
+
+                    if ((turn1 == Enums.TurnType.Right && turn2 != Enums.TurnType.Right))
+                        right = j;
+                }
+
+
+                List<Point> newHull = new List<Point>();
+
+                int idx = left;
+                newHull.Add(hull[idx]);
+
+                while (idx != right)
+                {
+                    idx = (idx + 1) % hull.Count;
+                }
+
+                newHull.Add(p);
+                newHull.Add(hull[right]);
+
+                idx = (right + 1) % hull.Count;
+                while (idx != left)
+                {
+                    newHull.Add(hull[idx]);
+                    idx = (idx + 1) % hull.Count;
+                }
+
+                hull = newHull;
+            }
+            List<Point> cleanedHull = new List<Point>();
+
+            int n = hull.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                Point prev = hull[(i - 1 + n) % n];
+                Point curr = hull[i];
+                Point next = hull[(i + 1) % n];
+
+                if (HelperMethods.CheckTurn(new Line(prev, curr), next) == Enums.TurnType.Colinear)
+                {
+
+                    if (HelperMethods.PointOnSegment(curr, prev, next))
+                    {
+                        continue;
+                    }
+                }
+
+                cleanedHull.Add(curr);
+            }
+
+            hull = cleanedHull;
+            for (int i = 0; i < hull.Count; i++)
+            {
+                if (!outPoints.Contains(hull[i]))
+                    outPoints.Add(hull[i]);
+            }
+
+        }
         public override string ToString()
         {
             return "Convex Hull - Incremental";
